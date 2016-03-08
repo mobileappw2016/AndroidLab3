338f0014921c34d9c5bb7dbf3051e7dfbbb4aa36
AriYB <artiger61@gmail.com>
2016-03-08 17:07:42 -0500

    Adding stuff

--- TipCalculator/app/src/main/java/com/example/aribu/tipcalculator/MainActivity.java
+++ TipCalculator/app/src/main/java/com/example/aribu/tipcalculator/MainActivity.java
@@ -8,24 +8,32 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    Button num1;
+    Button num2;
+    Button num3;
+    Button num4;
+    Button num5;
+    Button num6;
+    Button num7;
+    Button num8;
+    Button num9;
+    Button del;
+    Button dot;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+
     }
 
     @Override
